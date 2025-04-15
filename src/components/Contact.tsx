c1b481f4a548787f6ff9a916b9bc24bc8f2e60c1
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2025-04-15 23:46:13 +0100

    feat: enhance Contact and Experience components with updated content and styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,14 +6,14 @@ function Contact() {
         <div className="p-10">
             <h1 className="text-4xl text-center font-bold">Connect with me</h1>
         </div>
-        <div className="p-10 grid grid-cols-2 gap-10 items-center">
+        <div className="py-10 px-50 grid grid-cols-2 gap-10 items-center">
             
             <div className="col-span-1 px-10">
                 <p className="text-2xl mb-10">I&rsquo;d love to hear from you! Whether you have a question, a project in mind, or just want to say hi, feel free to reach out.</p>
             </div>
-            
+
             <div className="col-span-1 px-10">
-                <p className="text-2xl mb-10">You can also find me on:</p>
+                <p className="text-2xl mb-1">You can also find me on:</p>
                 <ul className="text-2xl mb-10">
                     <li><a href="#" className="text-blue-500">LinkedIn</a></li>
                     <li><a href="#" className="text-blue-500">GitHub</a></li>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,54 +10,62 @@ function Experience() {
 
                 <details open className="group py-1 text-lg">
                     <summary className="flex mb-5 cursor-pointer items-center justify-between py-1 font-semibold text-white marker:[font-size:0px]">
-                        Nohat LLC
-                        <div className="w-70 flex"> 
+                        Partner & CTO @ Nohat LLC
+                        <div className="w-60 flex justify-between"> 
                             <div className="px-10">2023 - Present</div> 
                             <svg className="h-6 w-6 rotate-0 transform text-white group-open:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
                             </svg>
                         </div>
                     </summary>
-                    <p className="text-gray-500">This demo was built by <a className="text-blue-500 hover:text-blue-700" href="https://marktownsend.rocks">Mark Townsend</a>, a professional web developer with a passion for Laravel and Tailwind CSS.</p>
+                    <p className="text-white bg-gray-800 p-5 rounded-lg">
+                        Since 2023, I&lsquo;ve served as the CTO of Nohat LLC, spearheading the development of cutting-edge security solutions. My focus includes endpoint protection, anti-phishing technologies, and other solutions leveraging AI to enhance threat detection and incident response capabilities.
+                    </p>
                 </details>
 
                 <details className="group py-1 text-lg">
-                    <summary className="flex cursor-pointer flex-row items-center justify-between py-1 font-semibold text-gray-800 marker:[font-size:0px]">
-                        SmartRentics Inc
-                        <div className="w-20 flex"> 
-                            <div>2022 - 2024</div> 
+                    <summary className="flex mb-5 cursor-pointer flex-row items-center justify-between py-1 font-semibold text-white marker:[font-size:0px]">
+                        Partner & CTO @ SmartRentics Inc
+                        <div className="w-60 flex justify-between"> 
+                            <div className="px-10">2022 - 2024</div> 
                             <svg className="h-6 w-6 rotate-0 transform text-white group-open:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
                             </svg>
                         </div>
                     </summary>
-                    <p className="text-gray-500">It&lsquo;s true, you could just sprinkle in some Javascript. Many modern Javascript libraries are incredibly easy to use. But sometimes you don&lsquo;t need Javascript when you can solve your problem with simple HTML and CSS.</p>
+                    <p className="text-white bg-gray-800 p-5 rounded-lg">
+                        As Partner and CTO of SmartRentics Inc, I led the development of a machine learning-powered AI platform that improves the apartment hunting process. The system leveraged advanced savings algorithms to help users save time, money, and effort, transforming how people find and secure rental properties.
+                    </p>
                 </details>
 
                 <details className="group py-1 text-lg">
-                    <summary className="flex cursor-pointer flex-row items-center justify-between py-1 font-semibold text-gray-800 marker:[font-size:0px]">
-                        Maiel Technologies LLC
-                        <div className="w-20 flex"> 
-                            <div>2020 - Present</div> 
+                    <summary className="flex mb-5 cursor-pointer flex-row items-center justify-between py-1 font-semibold text-white marker:[font-size:0px]">
+                        Founder @ Maiel Technologies LLC
+                        <div className="w-60 flex justify-between"> 
+                            <div className="px-10">2020 - Present</div> 
                             <svg className="h-6 w-6 rotate-0 transform text-white group-open:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
                             </svg>
                         </div>
                     </summary>
-                    <p className="text-gray-500">Absolutely! I think sharing ideas (and sometimes code) are invaluable to the learning process. And let&lsquo;s face it: sometimes you just want to get a simple FAQ or accordion section up fast and move on to more pressing parts of whatever you&lsquo;re building.</p>
+                    <p className="text-white bg-gray-800 p-5 rounded-lg">
+                        As the Founder of Maiel Technologies LLC, I established a web development and maintenance agency dedicated to helping SMEs build and maintain their online presence. Through innovative solutions and personalized support, we empower businesses to create professional, high-performing websites that drive growth and engagement.
+                    </p>
                 </details>
 
                 <details className="group py-1 text-lg">
-                    <summary className="flex cursor-pointer flex-row items-center justify-between py-1 font-semibold text-gray-800 marker:[font-size:0px]">
-                        Roundstone Consulting
-                        <div className="w-20 flex"> 
-                            <div>2017 - 2023</div> 
+                    <summary className="flex mb-5 cursor-pointer flex-row items-center justify-between py-1 font-semibold text-white marker:[font-size:0px]">
+                        Software Engineer @ Roundstone Consulting
+                        <div className="w-60 flex justify-between"> 
+                            <div className="px-10">2017 - 2023</div> 
                             <svg className="h-6 w-6 rotate-0 transform text-white group-open:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"></path>
                             </svg>
                         </div>
                     </summary>
-                    <p className="text-gray-500">Absolutely! I think sharing ideas (and sometimes code) are invaluable to the learning process. And let&lsquo;s face it: sometimes you just want to get a simple FAQ or accordion section up fast and move on to more pressing parts of whatever you&lsquo;re building.</p>
+                    <p className="text-white bg-gray-800 p-5 rounded-lg">
+                    As a Software Engineer at Roundstone Consulting, I contributed to the design and development of innovative software solutions tailored to solve real-world business challenges. Working closely with small and medium-sized firms, I helped create custom applications and ICT solutions that empowered clients to grow their businesses and achieve their goals.
+                    </p>
                 </details>
             </section>
 
